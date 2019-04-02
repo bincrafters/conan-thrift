bdd0988ba9c8ac873ff4656e5487676394a253d3
Uilian Ries <uilianries@gmail.com>
2019-04-02 15:02:58 -0300

    Update Conan recipe
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,34 +1,20 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-from conans import ConanFile, CMake, tools, AutoToolsBuildEnvironment
 import os
+from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
 
 
 class ThriftConan(ConanFile):
     name = "thrift"
     version = "0.12.0"
-    description =   "Thrift is a lightweight, \
-                    language-independent software \
-                    stack with an associated code \
-                    generation mechanism for RPC."
+    description = "Thrift is an associated code generation mechanism for RPC"
     url = "https://github.com/helmesjo/conan-thrift"
-    homepage = "https://thrift.apache.org/"
+    homepage = "https://github.com/apache/thrift"
     author = "helmesjo <helmesjo@gmail.com>"
-    license = "Apache License 2.0"
+    license = "Apache-2.0"
     exports = ["LICENSE.md"]
-
     exports_sources = ["CMakeLists.txt"]
     generators = "cmake"
-
-    source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
-
-    # http://thrift.apache.org/docs/install/
-    requires = (
-        "boost/1.67.0@conan/stable",
-    )
-
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
@@ -51,94 +37,78 @@ class ThriftConan(ConanFile):
         "with_plugin": [True, False],
         "build_libraries": [True, False],
         "build_compiler": [True, False],
-        "build_testing": [True, False],
         "build_examples": [True, False],
-        "build_tutorials": [True, False],
+        "build_tutorials": [True, False]
     }
-
-    default_options = (
-        "shared=False",
-        "fPIC=True",
-        "with_zlib=True",
-        "with_libevent=True",
-        "with_qt4=False",
-        "with_qt5=False",
-        "with_openssl=True",
-        "with_boost_functional=False",
-        "with_boost_smart_ptr=False",
-        "with_boost_static=False",
-        "with_boostthreads=False",
-        "with_stdthreads=True",
-        "with_c_glib=False",
-        "with_cpp=True",
-        "with_java=False",
-        "with_python=False",
-        "with_haskell=False",
-        "with_plugin=False",
-        "build_libraries=True",
-        "build_compiler=True",
-        "build_testing=False", # Currently fails if 'True' because of too recent boost::test version (?) in package
-        "build_examples=False",
-        "build_tutorials=False",
-    )
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "with_zlib": True,
+        "with_libevent": True,
+        "with_qt4": False,
+        "with_qt5": False,
+        "with_openssl": True,
+        "with_boost_functional": False,
+        "with_boost_smart_ptr": False,
+        "with_boost_static": False,
+        "with_boostthreads": False,
+        "with_stdthreads": True,
+        "with_c_glib": False,
+        "with_cpp": True,
+        "with_java": False,
+        "with_python": False,
+        "with_haskell": False,
+        "with_plugin": False,
+        "build_libraries": True,
+        "build_compiler": True,
+        "build_examples": False,
+        "build_tutorials": False,
+    }
+    _source_subfolder = "source_subfolder"
+    _build_subfolder = "build_subfolder"
 
     def config_options(self):
         if self.settings.os == 'Windows':
             del self.options.fPIC
 
     def configure(self):
-        if self.settings.compiler != 'Visual Studio' and self.options.shared:
-            self.options['boost'].add_option('fPIC', 'True')
-
-        # See: https://github.com/apache/thrift/blob/f12cacf56145e2c8f0d4429694fedf5453648089/build/cmake/DefinePlatformSpecifc.cmake
+        # See: https://github.com/apache/thrift/blob/v0.12.0/build/cmake/DefinePlatformSpecifc.cmake
         if self.settings.os == "Windows" and self.options.shared:
-            self.output.warn("Thrift does not currently support shared libs on windows. Forcing static...")
-            self.options.shared = False
+            raise ConanInvalidConfiguration("Thrift does not currently support shared libs on windows. Forcing static...")
 
     def requirements(self):
+        self.requires("boost/1.68.0@conan/stable")
         if self.settings.os == 'Windows':
-            self.requires("winflexbison/2.5.14@helmesjo/stable")
+            self.requires("winflexbison/2.5.17@bincrafters/stable")
         else:
             self.requires("flex/2.6.4@bincrafters/stable")
-            self.requires("bison/3.0.4@bincrafters/stable")
-            
+            self.requires("bison/3.0.5@bincrafters/stable")
+
         if self.options.with_openssl:
-            self.requires("OpenSSL/1.1.0g@conan/stable")
+            self.requires("OpenSSL/1.0.2q@conan/stable")
         if self.options.with_zlib:
             self.requires("zlib/1.2.11@conan/stable")
         if self.options.with_libevent:
-            self.requires("libevent/2.0.22@bincrafters/stable")
+            self.requires("libevent/2.1.8@bincrafters/stable")
 
     def source(self):
-        source_url = "https://github.com/apache/thrift"
-        tools.get("{0}/archive/{1}.tar.gz".format(source_url, self.version))
+        sha256 = "b7452d1873c6c43a580d2b4ae38cfaf8fa098ee6dc2925bae98dce0c010b1366"
+        tools.get("{0}/archive/{1}.tar.gz".format(self.homepage, self.version), sha256=sha256)
         extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self._source_subfolder)
 
-        #Rename to "source_subfolder" is a convention to simplify later steps
-        os.rename(extracted_dir, self.source_subfolder)
-
-    def configure_cmake(self):
-        def add_cmake_option(option, value):
-            var_name = "{}".format(option).upper()
-            value_str = "{}".format(value)
-            var_value = "ON" if value_str == 'True' else "OFF" if value_str == 'False' else value_str 
-            cmake.definitions[var_name] = var_value
-
+    def _configure_cmake(self):
         cmake = CMake(self)
-
-        if self.settings.os != 'Windows':
-            cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
-
         for option, value in self.options.items():
-            add_cmake_option(option, value)
+            cmake.definitions[option.upper()] = value
 
         # Make thrift use correct thread lib (see repo/build/cmake/config.h.in)
-        add_cmake_option("USE_STD_THREAD", self.options.with_stdthreads)
-        add_cmake_option("USE_BOOST_THREAD", self.options.with_boostthreads)
-
-        add_cmake_option("WITH_SHARED_LIB", self.options.shared)
-        add_cmake_option("WITH_STATIC_LIB", not self.options.shared)
+        cmake.definitions["USE_STD_THREAD"] = self.options.with_stdthreads
+        cmake.definitions["USE_BOOST_THREAD"] = self.options.with_boostthreads
+        cmake.definitions["WITH_SHARED_LIB"] = self.options.shared
+        cmake.definitions["WITH_STATIC_LIB"] = not self.options.shared
         cmake.definitions["BOOST_ROOT"] = self.deps_cpp_info['boost'].rootpath
+        cmake.definitions["BUILD_TESTING"] = False
 
         # Make optional libs "findable"
         if self.options.with_openssl:
@@ -148,30 +118,24 @@ class ThriftConan(ConanFile):
         if self.options.with_libevent:
             cmake.definitions["LIBEVENT_ROOT"] = self.deps_cpp_info['libevent'].rootpath
 
-        cmake.configure(build_folder=self.build_subfolder)
+        cmake.configure(build_folder=self._build_subfolder)
         return cmake
 
     def build(self):
-        cmake = self.configure_cmake()
+        cmake = self._configure_cmake()
         cmake.build()
 
-        if self.options.build_testing:
-            self.output.info("Running {} tests".format(self.name))
-            source_path = os.path.join(self.build_subfolder, self.source_subfolder)
-            with tools.chdir(source_path):
-                self.run("ctest --build-config {}".format(self.settings.build_type))
-        
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
-        cmake = self.configure_cmake()
+        self.copy(pattern="LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
         cmake.install()
-        build_source_dir = os.path.join(self.build_subfolder, self.source_subfolder)
         # Copy generated headers from build tree
+        build_source_dir = os.path.join(self._build_subfolder, self._source_subfolder)
         self.copy(pattern="*.h", dst="include", src=build_source_dir, keep_path=True)
 
     def package_info(self):
         # Make 'thrift' compiler available to consumers
-        self.env_info.path.append(os.path.join(self.package_folder, "bin"))
+        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
         self.cpp_info.libs = tools.collect_libs(self)
         # Make sure libs are link in correct order. Important thing is that libthrift/thrift is last
         # (a little naive to sort, but libthrift/thrift should end up last since rest of the libs extend it with an abbrevation: 'thriftnb', 'thriftz')
@@ -179,4 +143,7 @@ class ThriftConan(ConanFile):
         self.cpp_info.libs.sort(reverse = True)
 
         if self.settings.os == "Windows":
-            self.cpp_info.defines.append("NOMINMAX") # To avoid error C2589: '(' : illegal token on right side of '::'
+            # To avoid error C2589: '(' : illegal token on right side of '::'
+            self.cpp_info.defines.append("NOMINMAX")
+        elif self.settings.os == "Linux":
+            self.cpp_info.libs.extend(["m", "pthread"])
