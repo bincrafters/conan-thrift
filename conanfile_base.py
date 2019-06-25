ad52483d321c76b7896dcbc7abf01916ba54cbc5
daniel haensse <daniel.haensse@swissembedded.com>
2019-06-25 16:39:19 +0200

    adding patches

--- conanfile_base.py
+++ conanfile_base.py
@@ -8,14 +8,13 @@ class ConanFileBase(ConanFile):
     version = "0.12.0"
     description = "Thrift is an associated code generation mechanism for RPC"
     topics = ("conan", "thrift", "serialization", "rpc")
-    url = "https://github.com/bincrafters/conan-thrift"
+    url = "https://github.com/swissembedded/conan-thrift"
     homepage = "https://github.com/apache/thrift"
-    author = "helmesjo <helmesjo@gmail.com>"
+    author = "Daniel Haensse <daniel.haensse@swissembedded.com>"
     license = "Apache-2.0"
-    exports = ["LICENSE.md", "conanfile_base.py"]
+    exports = ["LICENSE.md", "conanfile_base.py", "patches/thrift-0.12.0_t_cpp_generator_struct_less_operator.patch", "patches/thrift-0.12.0_t_cpp_generator_struct_less_operator_DebugProtoTest_extras.cpp.patch", "patches/thrift-0.12.0_t_cpp_generator_struct_operator_less_ThriftTest_extras.patch"]
     exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
-
+    generators = "cmake"    
     _source_subfolder = "source_subfolder"
     _build_subfolder = "build_subfolder"
 
@@ -26,6 +25,14 @@ class ConanFileBase(ConanFile):
         os.rename(extracted_dir, self._source_subfolder)
 
     def build(self):
+        _patch_source_dir = os.path.join(self.source_folder, self._source_subfolder)
+        print(_patch_source_dir)
+        # patching cpp struct less operator
+        tools.patch(_patch_source_dir, os.path.join(self.source_folder,"patches/thrift-0.12.0_t_cpp_generator_struct_less_operator.patch"))
+        # patching tests
+        tools.patch(_patch_source_dir, os.path.join(self.source_folder, "patches/thrift-0.12.0_t_cpp_generator_struct_less_operator_DebugProtoTest_extras.cpp.patch"))
+        tools.patch(_patch_source_dir, os.path.join(self.source_folder, "patches/thrift-0.12.0_t_cpp_generator_struct_operator_less_ThriftTest_extras.patch"))
+
         cmake = self._configure_cmake()
         cmake.build()
 
